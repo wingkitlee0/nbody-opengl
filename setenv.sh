af83abec2ae5e200f823b839c80c06e95b15c3ae
Kit Lee <wingkitlee0@outlook.com>
2018-08-13 16:03:06 -0500

    update

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# to fix the error ...
+# https://stackoverflow.com/questions/41338549/sdl2-cant-create-window-since-it-couldnt-find-matching-glx-visual
+
+export SDL_VIDEO_X11_VISUALID=
